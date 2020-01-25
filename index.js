f04c5daae51b1bd74a8682fd35197c3eed1522d8
toshinori sato <tyahhahha@gmail.com>
2020-01-26 00:41:01 +0900

    add photo type

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+  # Photo型を定義します
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
 
   type Mutation {
-    postPhoto(name: String!, description: String): Boolean!
+    postPhoto(name: String!, description: String): Photo!
   }
 `;
 
+let _id = 0;
 const photos = [];
 
 const resolvers = {
   Query: {
     // 写真を格納した配列の長さを変えs
-    totalPhotos: () => photos.length
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
   },
 
   // postPhotoミューテーションと対応するリゾルバ
   Mutation: {
     postPhoto(parent, args) {
-      console.log("paret", parent);
-      photos.push(args);
-      return true;
+      const newPhoto = {
+        id: _id++,
+        ...args
+      };
+      photos.push(newPhoto);
+      return newPhoto;
     }
+  },
+  Photo: {
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
   }
 };
 
