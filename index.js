496c3686ffc8782a55a6ddabad367aef9fb1212d
toshinori sato <tyahhahha@gmail.com>
2020-01-26 21:49:51 +0900

    add tag(link user to photo)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const typeDefs = `
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   enum PhotoCategory {
@@ -23,6 +24,7 @@ const typeDefs = `
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
   }
 
   type Query {
@@ -71,6 +73,13 @@ const photos = [
   }
 ];
 
+const tags = [
+  { photoID: "1", userID: "gPlake" },
+  { photoID: "2", userID: "gPlake" },
+  { photoID: "2", userID: "sSchmidt" },
+  { photoID: "2", userID: "mHattrup" }
+];
+
 const resolvers = {
   Query: {
     // 写真を格納した配列の長さを変えs
@@ -93,11 +102,23 @@ const resolvers = {
     url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
     postedBy(parent) {
       return users.find(u => u.githubLogin === parent.githubUser);
+    },
+    taggedUsers(parent) {
+      return tags
+        .filter(tag => tag.photoID === parent.id)
+        .map(tag => tag.userID)
+        .map(userID => users.find(u => u.githubLogin === userID));
     }
   },
   User: {
     postedPhotos(parent) {
       return photos.filter(p => p.githubUser === parent.githubLogin);
+    },
+    inPhotos(parent) {
+      return tags
+        .filter(tag => tag.userID === parent.id)
+        .map(tag => tag.photoID)
+        .map(photoID => photos.find(p => p.id === photoID));
     }
   }
 };
