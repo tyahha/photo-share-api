a6a5d31fda9acf9f4cebe39808a8c0d835b1e7a6
toshinori sato <tyahhahha@gmail.com>
2020-02-10 23:19:08 +0900

    authize with github

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,4 +1,5 @@
 const { _id, photos } = require("../data");
+const { authorizeWithGithub } = require("../lib");
 
 module.exports = {
   postPhoto(parent, args) {
@@ -9,5 +10,37 @@ module.exports = {
     };
     photos.push(newPhoto);
     return newPhoto;
+  },
+  async githubAuth(parent, { code }, { db }) {
+    const {
+      message,
+      access_token,
+      avatar_url,
+      login,
+      name
+    } = await authorizeWithGithub({
+      client_id: "e066004cbfb0e8d2b533",
+      client_secret: "84559dd1d138c7482b75f9b07350f7e261300441",
+      code
+    });
+
+    if (message) {
+      throw new Error(message);
+    }
+
+    const latestUserInfo = {
+      name,
+      githubLogin: login,
+      githubToken: access_token,
+      avatar: avatar_url
+    };
+
+    const {
+      ops: [user]
+    } = await db
+      .collection("users")
+      .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true });
+
+    return { user, token: access_token };
   }
 };
