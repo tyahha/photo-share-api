6876756b30226fc87a81eeca420a33762a52121b
toshinori sato <tyahhahha@gmail.com>
2020-02-11 18:19:48 +0900

    add fakeUser

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,5 +1,6 @@
 const { _id, photos } = require("../data");
 const { authorizeWithGithub } = require("../lib");
+const fetch = require("node-fetch");
 
 module.exports = {
   async postPhoto(parent, args, { db, currentUser }) {
@@ -49,5 +50,32 @@ module.exports = {
       .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true });
 
     return { user, token: access_token };
+  },
+  addFakeUsers: async (root, { count }, { db }) => {
+    const randomeUserApi = `https://randomuser.me/api/?results=${count}`;
+    const { results } = await fetch(randomeUserApi).then(res => res.json());
+
+    const users = results.map(r => ({
+      githubLogin: r.login.username,
+      name: `${r.name.first} ${r.name.last}`,
+      avatar: r.picture.thumbnail,
+      githubToken: r.login.sha1
+    }));
+
+    await db.collection("users").insert(users);
+
+    return users;
+  },
+  fakeUserAuth: async (parent, { githubLogin }, { db }) => {
+    const user = await db.collection("users").findOne({ githubLogin });
+
+    if (!user) {
+      throw new Error(`Cannot find user with githubLogin ${githubLogin}`);
+    }
+
+    return {
+      token: user.githubLogin,
+      user
+    };
   }
 };
